bd799c011836e770c9279b619ae186ded87a343b
Daniel Rondeau <Daniel.Rondeau@vera.com.uy>
2024-02-25 14:35:51 -0300

    Jugando con un script para crear usuarios y compartir las key.
    Me tengo que bajar mucho los pantalones para crear los usuarios y pasar los key, por eso voy por el camiono de generar un bash.

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define variables
+LOCAL_USERNAME="ansible"
+REMOTE_USERNAME="administrator"
+REMOTE_SERVERS_FILE="remote_servers.txt"
+
+# Check if the remote servers file exists
+if [ ! -f "$REMOTE_SERVERS_FILE" ]; then
+    echo "Remote servers file $REMOTE_SERVERS_FILE not found."
+    exit 1
+fi
+
+# Create local user
+sudo useradd -m $LOCAL_USERNAME
+sudo mkdir -p /home/ansible/.ssh/
+
+# Generate SSH key pair (Ed25519) for local user
+sudo ssh-keygen -t ed25519 -f "/home/$LOCAL_USERNAME/.ssh/id_ed25519" -N ""
+sudo cp "/home/$LOCAL_USERNAME/.ssh/id_ed25519" "~/certificates"
+
+# Copy the public key to remote servers
+while IFS= read -r SERVER; do
+    sudo ssh-copy-id -i /home/$LOCAL_USERNAME/.ssh/id_ed25519.pub $REMOTE_USERNAME@$SERVER
+done < "$REMOTE_SERVERS_FILE"
+
+echo "Local user $LOCAL_USERNAME created and SSH keys (Ed25519) copied to remote servers."
+
