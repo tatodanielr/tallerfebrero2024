ba1503b4b1f9ccfb740d9e3ee22a4250f3f12b82
tatodanielr <157429072+tatodanielr@users.noreply.github.com>
2024-02-27 00:16:45 -0300

    Es muy tarde y estoy haciendo error de pija.
    Vamos que sale.

--- setup_administrator_ssh_key.sh
+++ setup_administrator_ssh_key.sh
@@ -61,15 +61,15 @@ fi
 
 for HOST in "${SERVERS[@]}"; do
     echo "Creating user ansible on $HOST..."
+    ssh $USER_Adm@$HOST "echo 'ansible:$USER_Ansible' | sudo chpasswd"
+    REMOTE_SERVER_DISTRIBUTION="$(cat /etc/os-release | grep "^NAME" | cut -d "=" -f 2 | sed 's/"//g')"
     if [ "$REMOTE_SERVER_DISTRIBUTION" = "Ubuntu" ]; then
     ssh $USER_Adm@$HOST "sudo useradd ansible -m -s /bin/bash"
-    REMOTE_SERVER_DISTRIBUTION="$(cat /etc/os-release | grep "^NAME" | cut -d "=" -f 2 | sed 's/"//g')"
     ssh $USER_Adm@$HOST "sudo usermod -aG sudo $USER_Ansible"
     ssh $USER_Adm@$HOST "sudo mkdir /home/ansible/.ssh"
     ssh $USER_Adm@$HOST "echo 'ansible:$USER_Ansible' | sudo chpasswd"
     elif [ "$REMOTE_SERVER_DISTRIBUTION" = "Rocky" ]; then
     ssh $USER_Adm@$HOST "sudo useradd ansible -m -s /bin/bash"
-    REMOTE_SERVER_DISTRIBUTION="$(cat /etc/os-release | grep "^NAME" | cut -d "=" -f 2 | sed 's/"//g')"
     ssh $USER_Adm@$HOST "sudo usermod -aG wheel $USER_Ansible"
     ssh $USER_Adm@$HOST "sudo mkdir /home/ansible/.ssh"
     ssh $USER_Adm@$HOST "echo 'ansible:$USER_Ansible' | sudo chpasswd"
