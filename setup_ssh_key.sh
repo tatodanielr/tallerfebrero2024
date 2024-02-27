f49aef678a2ad506adb096472a711fe6eef8e9fd
tatodanielr <157429072+tatodanielr@users.noreply.github.com>
2024-02-26 22:26:58 -0300

    Pase un archivo por arriba.

--- setup_ssh_key.sh
+++ setup_ssh_key.sh
@@ -13,8 +13,8 @@ fi
 USER="administrator"
 
 # Prompt user for SSH password (it will be used for all SSH connections)
-#read -s -p "Enter SSH password for $USER: " SSH_PASSWORD
-#echo
+read -s -p "Enter SSH password for $USER: " SSH_PASSWORD
+echo
 
 # Path to the local SSH public key
 SSH_PUBLIC_KEY="$HOME/.ssh/id_ed25519.pub"
@@ -23,14 +23,15 @@ SSH_PUBLIC_KEY="$HOME/.ssh/id_ed25519.pub"
 if [ ! -f "$SSH_PUBLIC_KEY" ]; then
     echo "Local SSH public key '$SSH_PUBLIC_KEY' does not exist."
     echo "Generate SSH key pair first using 'ssh-keygen -t ed25519' command."
+    # Generate SSH Ed25519 key pair with a comment
+    ssh-keygen -t ed25519 -C "Administrator@bastion"
     exit 1
 fi
 
 # Read the list of servers from the file
 mapfile -t SERVERS < "$SERVERS_FILE"
 
-# Generate SSH Ed25519 key pair with a comment
-ssh-keygen -t ed25519 -C "SSH Key for server access"
+
 
 # Copy the public key to remote servers
 for HOST in "${SERVERS[@]}"; do
